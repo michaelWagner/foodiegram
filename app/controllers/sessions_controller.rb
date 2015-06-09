fef4f86e9a205998b944bdfb4595d5a84238a455
Michael Wagner <wagnerdmike@gmail.com>
2015-06-09 14:58:57 -0700

    Add show basic user show page and more auth.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if @user
       sign_in!(@user)
-      redirect_to root_url
+      redirect_to @user
     else
       flash.now[:errors] = ["Invalid login credentials"]
       render :new
