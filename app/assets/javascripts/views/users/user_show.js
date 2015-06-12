a6fd13f0c61a9c1e6790e9b6e8be6a377e7e013d
Michael Wagner <wagnerdmike@gmail.com>
2015-06-12 13:35:43 -0700

    Add edit profile button.

--- app/assets/javascripts/views/users/user_show.js
+++ app/assets/javascripts/views/users/user_show.js
@@ -1,8 +1,17 @@
 Foodiegram.Views.UserShow = Backbone.View.extend({
   template: JST['users/show'],
 
+  events: {
+    "click .edit-profile-button": "editProfile"
+  },
+
   initialize: function() {
     this.listenTo(this.model, "sync", this.render);
+  },
+
+  editProfile: function() {
+    Backbone.history
+      .navigate("#/users/" + CURRENT_USER_ID + "/edit", { trigger: true });
   },
 
   render: function() {
--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! user, :username, :id, :created_at, :updated_at, :description
+json.extract! @user, :username, :id, :created_at, :updated_at, :description
