4db0e01c3d0a40b4185df8ad6d4141818f917165
Michael Wagner <wagnerdmike@gmail.com>
2015-06-12 18:10:52 -0700

    Add comparator for posts.

--- app/assets/javascripts/collections/posts.js
+++ app/assets/javascripts/collections/posts.js
@@ -1,6 +1,9 @@
 Foodiegram.Collections.Posts = Backbone.Collection.extend({
   url: 'api/posts',
   model: Foodiegram.Models.Post,
+  comparator: function(model) {
+    return -(model.get('updated_at'));
+  },
 
   initialize: function(models, options) {
     if (options && options.user) {
