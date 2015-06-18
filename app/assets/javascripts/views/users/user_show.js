72f21bb76efe77f157a545b43385a8e44bba4d1e
Michael Wagner <wagnerdmike@gmail.com>
2015-06-18 02:17:31 -0700

    Fix follow button rendering.

--- app/assets/javascripts/views/users/user_show.js
+++ app/assets/javascripts/views/users/user_show.js
@@ -8,8 +8,22 @@ Foodiegram.Views.UserShow = Backbone.View.extend({
 
   initialize: function() {
     this.listenTo(this.model, "sync", this.render);
-    this.listenTo(this.model.followers(), "remove add", this.render);
-    this.listenTo(this.model.followings(), "add", this.render);
+  },
+
+  addButton: function() {
+    var button = $('<button>').addClass('btn btn-primary');
+    if (this.model.id === CURRENT_USER_ID) {
+      button.addClass('edit-profile-button')
+        .text('Edit Profile');
+    } else {
+      button.addClass('follow-user-button');
+      if (this.model.followers().get(CURRENT_USER_ID)) {
+        button.text('Unfollow');
+      } else {
+        button.text('Follow');
+      }
+    }
+    $('.profile-button').html(button);
   },
 
   editProfile: function() {
@@ -24,23 +38,24 @@ Foodiegram.Views.UserShow = Backbone.View.extend({
     return this._follow;
   },
 
-  toggleFollow: function(event) {
-    event.preventDefault();
-    if (this.model.id !== CURRENT_USER_ID) {
-      if (this.model.followers().get(CURRENT_USER_ID)) {
-        var following = new Foodiegram.Collections.Followings();
-        this._follow = following.getOrFetch(CURRENT_USER_ID);
-        this.destroyFollow();
-      } else {
-        this.createFollow();
-      }
+  toggleFollow: function() {
+    if (this.model.followers().get(CURRENT_USER_ID)) {
+      var following = new Foodiegram.Collections.Followings();
+      this._follow = following.getOrFetch(CURRENT_USER_ID);
+      this.destroyFollow();
+    } else {
+      this._follow = null;
+      this.createFollow();
     }
+    $('.follow-user-button').remove();
+    this.render();
   },
 
   createFollow: function () {
     this.follow().save({ followed_id: this.model.id }, {
       success: function () {
         this.model.followers().add(this.follow());
+        this.model.fetch();
         // $(".profile-button").text("Unfollow");
       }.bind(this)
     });
@@ -50,6 +65,7 @@ Foodiegram.Views.UserShow = Backbone.View.extend({
     this.follow().destroy({
       success: function() {
         this.model.followers().remove(this.follow());
+        this.model.fetch();
       }.bind(this)
     });
   },
@@ -58,7 +74,9 @@ Foodiegram.Views.UserShow = Backbone.View.extend({
     var content = this.template({
       user: this.model
     });
+
     this.$el.html(content);
+    this.addButton();
     return this;
   }
 });
